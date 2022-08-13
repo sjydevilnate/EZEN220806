25cb56835698ca83e9c01cb4aa6ad680ed6206c0
sejoong <sjydevil@gmail.com>
2022-08-13 11:31:06 +0900

    22-08-13

---
+++ ES2015문법/ap06.ES2015문법-cli/es2015test/build/ap06-01.js
@@ -0,0 +1,11 @@
+'use strict';
+
+debugger;
+var name = 'world';
+console.log("Hello ".concat(name));
+
+var getMessage = function getMessage(params) {
+  console.log("Hello ".concat(params));
+};
+
+getMessage('sejoong');
