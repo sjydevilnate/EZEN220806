a43af109d2461bcfe93b2319589aee4c60a69053
sejoong <sjydevil@gmail.com>
2022-09-03 16:52:04 +0900

    22-09-03

---
+++ React/rt22.컴포넌트실습.StarRating/starprj01/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import RatingContaier from "./RatingContaier";
+
+function App() {
+  return (
+    <div>
+        <h2>App</h2>
+        <RatingContaier />
+    </div>
+
+  );
+}
+
+export default App;
