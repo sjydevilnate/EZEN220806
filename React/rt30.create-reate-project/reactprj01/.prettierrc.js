1bda487794e8dccd7a71a750a75d357a664d62da
sejoong <sjydevil@gmail.com>
2022-10-01 16:30:23 +0900

    22-10-01

--- React/rt30.create-reate-project/reactprj01/.prettierrc.js
+++ React/rt30.create-reate-project/reactprj01/.prettierrc.js
@@ -8,7 +8,6 @@ module.exports = {
   trailingComma: 'all',
   proseWrap: 'always',
   bracketSpacing: true,
-  requireConfig: false,
   jsxBracketSameLine: false,
   jsxSingleQuote: false /*JSX 안에서는 " 을 사용 */,
   quoteProps: 'as-needed',
