913a797b9fb68da92f9761f13aef9446cb429af9
sejoong <sjydevil@gmail.com>
2022-10-15 19:01:05 +0900

    22-10-15

--- React/rt30.create-reate-project/reactprj01/src/utils/add.test.js
+++ React/rt30.create-reate-project/reactprj01/src/utils/add.test.js
@@ -1,11 +1,10 @@
-import 'jest-styled-components';
-import '@testing-library/jest-dom';
 import { add } from './add';
 
 describe('add.test 테스트', () => {
   test('add:: ', (done) => {
     // Assertions
     const aaa = add(1, 2);
+    debugger;
     expect(aaa).toBe(3); // true
     done();
   });
