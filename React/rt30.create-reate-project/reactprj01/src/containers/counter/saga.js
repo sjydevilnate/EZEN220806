b64e67c2de61c964182507cc291448e1847b04e4
sejoong <sjydevil@gmail.com>
2022-10-15 12:07:50 +0900

    22-10-15

--- React/rt30.create-reate-project/reactprj01/src/containers/counter/saga.js
+++ React/rt30.create-reate-project/reactprj01/src/containers/counter/saga.js
@@ -27,7 +27,7 @@ import * as api from './api';
 export function* getPromise(action) {
   try {
     yield put(actions.setLoading(true)); // 액션 디스패치
-    // yield delay(2000); // 2초 기다리기
+    yield delay(2000); // 2초 기다리기
     const result = yield call(api.getPromise, action.payload);
 
     // 액션함수를 이용하여 전달될 payload 생성
@@ -44,7 +44,7 @@ export function* getPromise(action) {
 export function* getTaskSaga(action) {
   try {
     yield put(actions.setLoading(true)); // 액션 디스패치
-    // yield delay(2000); // 2초 기다리기
+    yield delay(2000); // 2초 기다리기
     debugger;
     const result = yield call(api.callApi, action.payload);
     // 액션함수를 이용하여 전달될 payload 생성
@@ -62,7 +62,7 @@ export function* getTaskSaga(action) {
 export function* setTaskSaga(action) {
   try {
     yield put(actions.setLoading(true)); // 액션 디스패치
-    // yield delay(2000); // 2초 기다리기
+    yield delay(2000); // 2초 기다리기
     debugger;
     const result = yield call(api.callApi, action.payload);
     // 액션함수를 이용하여 전달될 payload 생성
